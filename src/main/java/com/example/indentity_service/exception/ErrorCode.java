b44fc062914fa6ec538e69acf7e0be8be4045c04
Nguyen Duc <dunguyen07022004@gmail.com>
2025-05-08 00:16:38 +0700

    update Validation nâng cao, trích xuất thông tin từ annotation

--- src/main/java/com/example/indentity_service/exception/ErrorCode.java
+++ src/main/java/com/example/indentity_service/exception/ErrorCode.java
@@ -6,14 +6,14 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     USER_EXISTED(1002, "User already existed",HttpStatus.INTERNAL_SERVER_ERROR),
     UNCATEGORIZED_EXCEPTION(9999, "UNCATEGORIZED_EXCEPTION",HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "USERNAME MUST BE AT LEAST 3 characters",HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1004,"PASSWORD MUST BE AT LEAST 8 characters",HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "USERNAME MUST BE AT LEAST {min} characters",HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004,"PASSWORD MUST BE AT LEAST {min} characters",HttpStatus.BAD_REQUEST),
     INVALID_KEY(1001,"Invalid key",HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1005, "USER_NOT_EXISTED",HttpStatus.NOT_FOUND),
     UNAUTHENICATION(1006, "AUTHENICATION",HttpStatus.UNAUTHORIZED),
     USER_NOT_FOUND(1007, " USER_NOT_FOUND" ,HttpStatus.NOT_FOUND),
     UNAUTHORIZED(1008, "You do not have permission" ,HttpStatus.FORBIDDEN),
-    INVALID_DOB(1009, "Your must be at least {}" ,HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1009, "Your must be at least {min}" ,HttpStatus.BAD_REQUEST),
     ;
     private int errorCode;
     private String errorMessage;
--- src/main/java/com/example/indentity_service/exception/GlobalException.java
+++ src/main/java/com/example/indentity_service/exception/GlobalException.java
@@ -2,6 +2,8 @@ package com.example.indentity_service.exception;
 
 import com.example.indentity_service.dto.ApiResponse;
 import jakarta.persistence.Column;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
@@ -9,23 +11,28 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalException {
-//    @ExceptionHandler(value = Exception.class)
-//    ResponseEntity<ApiResponse> runtimeExceptionHandler(Exception e) {
-//
-//        ApiResponse apiResponse = new ApiResponse();
-//        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getErrorCode());
-//        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getErrorMessage());
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
-//    }
+    private static final String MIN_ATTRIBUTE = "min";
+
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse> runtimeExceptionHandler(Exception e) {
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getErrorCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getErrorMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
+    }
 
 
     @ExceptionHandler(value = NoSuchElementException.class)
     ResponseEntity<ApiResponse> runtimeExceptionHandler(NoSuchElementException e) {
-        ErrorCode   errorCode = ErrorCode.USER_NOT_FOUND;
+        ErrorCode errorCode = ErrorCode.USER_NOT_FOUND;
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(ErrorCode.USER_NOT_FOUND.getErrorCode());
         apiResponse.setMessage(ErrorCode.USER_NOT_FOUND.getErrorMessage());
@@ -34,7 +41,7 @@ public class GlobalException {
 
     @ExceptionHandler(value = AccessDeniedException.class)
     ResponseEntity<ApiResponse> accessDeniedException(AccessDeniedException e) {
-        ErrorCode   errorCode = ErrorCode.UNAUTHORIZED;
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(ErrorCode.UNAUTHORIZED.getErrorCode());
         apiResponse.setMessage(ErrorCode.UNAUTHORIZED.getErrorMessage());
@@ -54,19 +61,32 @@ public class GlobalException {
     ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException e) {
         String enumKey = e.getFieldError().getDefaultMessage();
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
-
+        Map<String, Object> attributes = null;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
-
+            var constrainViolation = e.getBindingResult().getAllErrors().get(0).unwrap(ConstraintViolation.class);
+            attributes = constrainViolation.getConstraintDescriptor().getAttributes();
+            log.info(attributes.toString());
         } catch (IllegalArgumentException iae) {
 
         }
+
         ApiResponse apiResponse = new ApiResponse();
         apiResponse.setCode(errorCode.getErrorCode());
-        apiResponse.setMessage(errorCode.getErrorMessage());
+        apiResponse.setData(Objects.nonNull(attributes) ?
+                mapAttribute(errorCode.getErrorMessage(), attributes)
+                : errorCode.getErrorMessage()
+
+        );
+
 
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minAttribute = String.valueOf( attributes.get(MIN_ATTRIBUTE));
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minAttribute);
+
+    }
 
 }
